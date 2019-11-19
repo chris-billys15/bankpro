cf38735f5d83e6463f67346864be0d9d49d0f966
Christopher Billy <christopher.billys@gmail.com>
2019-11-19 18:12:55 +0700

    linted

--- src/Components/TransactionsComponent.js
+++ src/Components/TransactionsComponent.js
@@ -1,11 +1,9 @@
 // TransactionsComponent.js
 
 import React, { Component } from 'react';
-import { Table } from 'react-bootstrap';
 import BootstrapTable from 'react-bootstrap-table-next';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../CSS/Transactions.css'
-import {dark} from "@material-ui/core/styles/createPalette";
 class TransactionsComponent extends Component {
     constructor(props) {
         super(props);
@@ -62,53 +60,3 @@ class TransactionsComponent extends Component {
 }
 
 export default TransactionsComponent;
-
-/*
-<div style={{display:"flex"}}>
-                <div className="card-transactions">
-                    <h2>Transactions History</h2>
-                    <hr/>
-                    <Table responsive>
-                        <thead className="thead-dark">
-                        <tr>
-                            <th>No</th>
-                            <th>Waktu Transaksi</th>
-                            <th>Jenis Transaksi</th>
-                            <th>Jumlah Transaksi</th>
-                            <th>Rekening Terkait</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                            <td>Table cell</td>
-                        </tr>
-                        </tbody>
-                    </Table>
-                </div>
-            </div>
- */
-/*
-<div className="card-transactions">
-                <h2>Transactions History</h2>
-                <hr/>
-                <BootstrapTable keyField='id' data={ this.products } columns={ this.columns } />
-            </div>
- */
