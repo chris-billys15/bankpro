28366c254aa3547c867fc46ba630321d175a713c
Moch. Azhar Dhiaulhaq <13517047@std.stei.itb.ac.id>
2019-11-24 17:16:04 +0700

    Feature/ws integration

--- src/Components/HomeComponent.js
+++ src/Components/HomeComponent.js
@@ -1,8 +1,6 @@
 // Home.js
-// eslint-disable-next-line no-unused-vars
 import React, { Component } from 'react'
 
-// eslint-disable-next-line no-unused-vars
 import TransactionsComponent from './TransactionsComponent'
 import '../CSS/Transactions.css'
 import '../CSS/Home.css'
@@ -11,26 +9,41 @@ const formatter = new Intl.NumberFormat('en-US', {
   currency: 'IDR',
   minimumFractionDigits: 2
 })
+
 class HomeComponent extends Component {
   constructor (props) {
+    console.log(props);
     super(props)
     this.state = {
-      fullName: 'Christopher Billy Setiawan',
-      acc_no: '1234567890',
-      balance: '1000000'
+      fullName: '',
+      acc_no: null,
+      bank: '',
+      balance: '',
+      response: props.responseObject,
+      historyList: null
+    }
+    this.parseResponse = this.parseResponse.bind(this)
+  }
+  componentDidMount() {
+    this.parseResponse(this.state.response)
+  }
+  parseResponse(responseObj){
+    if (responseObj) {
+      console.log('aa');
+      this.setState({fullName: responseObj.return.namaPengguna, acc_no: responseObj.return.nomorNasabah, bank: responseObj.return.namaBank, balance: responseObj.return.saldo, historyList : responseObj.return.historyList});
     }
   }
 
   render () {
     return (
       <div className="container">
-        <div className="card">
+        <div className="card-home">
           <div className="welcome">Welcome to Bank Pro</div>
         </div>
         <div className="flex-container-row">
-          <div className="card">
+          <div className="card-home">
             <div className="flex-container-row">
-              <img src={require('../avatar.png')} alt={this.state.fullName} style={{ width: '80px', margin: '10px' }}/>
+              <img src={require('../avatar.png')} alt={this.state.fullName} style={{ width: '80px', margin: '10px 10px 10px 10px' }}/>
               <div className="flex-container-col">
                 <div className="fullName">
                   {this.state.fullName}
@@ -38,11 +51,14 @@ class HomeComponent extends Component {
                 <div style={{ alignSelf: 'left' }}>
                   Account No. : {this.state.acc_no}
                 </div>
+                <div style={{ alignSelf: 'left' }}>
+                  Bank : {this.state.bank}
+                </div>
               </div>
             </div>
           </div>
-          <div className="card" style={{ backgroundColor: '#e12159' }}>
-            <div className="flex-container-col">
+          <div className="card-home" style={{ backgroundColor: '#e12159'}}>
+            <div className="flex-container-col" style={{alignSelf:"center"}}>
               <div className="balance">
                 Total Balance
               </div>
@@ -52,7 +68,7 @@ class HomeComponent extends Component {
             </div>
           </div>
         </div>
-        <TransactionsComponent className="home-transactions"/>
+        <TransactionsComponent className="home-transactions" responseObject={this.state.response}/>
       </div>
     )
   }
--- src/Components/NavbarComponent.js
+++ src/Components/NavbarComponent.js
@@ -1,42 +1,82 @@
-import React, { useState } from 'react'
-import { Nav, NavItem, NavLink } from 'reactstrap'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import React from 'react'
+import {Nav} from 'reactstrap'
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom'
 import '../CSS/Navbar.css'
-
 import TransferComponent from './TransferComponent'
 import HomeComponent from '../Components/HomeComponent'
 import TransactionsComponent from './TransactionsComponent'
 import LoginComponent from '../Components/LoginComponent'
 
-const NavbarComponent = (props) => {
-  function getCookie () {
-    return localStorage.getItem('cookieBankPro')
-  }
-  function isCookieExist () {
-    return getCookie() != null
+export class NavbarComponent extends React.Component{
+
+  constructor(props){
+    super(props)
+    this.state= {
+      noRekening: localStorage.getItem("cookieBankPro"),
+      isAuthenticated: true,
+      responseObj: null
+    }
+    this.handleLogout = this.handleLogout.bind(this)
+    this.onCallback =this.onCallback.bind(this)
+    console.log("state: ",this.state.noRekening)
   }
 
-  return (
-    <div>
-      <Router>
-        <Nav tabs className="bar-wrapper" >
-          <div className="header-home"><b>Bank</b>Pro</div>
-          <div>
-            <Link to={'/home'} className="nav-link">Home</Link>
-            <Link to={'/transfer'} className="nav-link">Transfer</Link>
-            <Link to={'/transactions'} className="nav-link">Transactions</Link>
-            <Link to={'/login'} className="nav-link">Login</Link>
-          </div>
-        </Nav>
-        <Switch>
-          <Route exact path='/home' component={HomeComponent} />
-          <Route path='/transfer' component={TransferComponent} />
-          <Route path='/transactions' component={TransactionsComponent} />
-          <Route path='/login' component={LoginComponent} />
-        </Switch>
-      </Router>
-    </div>
-  )
+  onCallback(responseObject){
+    console.log(responseObject);
+    this.setState({responseObj: responseObject})
+  }
+
+  getProfile(AccNo, cb){
+    var soap = require('soap');
+    var url = 'http://100.26.43.243:8080/bankprowebservice-1.0-SNAPSHOT/NewWebService?wsdl';
+    var args = {rekening: AccNo};
+    soap.createClient(url, function(err, client) {
+      client.historyTransaction(args, function(err, result) {
+        cb(result)
+      });
+    });
+  }
+
+  componentDidMount() {
+    if(this.state.noRekening){
+      this.getProfile(this.state.noRekening, this.onCallback.bind(this));
+    }
+  }
+
+  handleLogout() {
+    localStorage.removeItem("cookieBankPro");
+    this.setState({isAuthenticated:false});
+    this.props.unRenderNavbar();
+  }
+
+  render() {
+    return (
+        <div>
+          <Router>
+            <Nav tabs className="bar-wrapper" >
+              <div className="header-home"><b>Bank</b>Pro</div>
+              <div>
+                <Link to={'/home'} className="nav-link">Home</Link>
+                <Link to={'/transfer'} className="nav-link">Transfer</Link>
+                <Link to={'/transactions'} className="nav-link">Transactions</Link>
+                <Link className="nav-link" onClick={this.handleLogout} to='/login'>Logout</Link>
+              </div>
+              {
+                (this.state.isAuthenticated === false) &&
+                <Redirect to='/login'/>
+              }
+            </Nav>
+            <Switch>
+              <Route exact path='/' component={() => <HomeComponent responseObject={this.state.responseObj}/>} />
+              <Route path='/home' component={() => <HomeComponent responseObject={this.state.responseObj}/>} />
+              <Route path='/transfer' component={() => <TransferComponent responseObject={this.state.responseObj}/>} />
+              <Route path='/transactions' component={() => <TransactionsComponent responseObject={this.state.responseObj}/>} />
+              <Route path='/login' component={() => <LoginComponent/>} />
+            </Switch>
+          </Router>
+        </div>
+    );
+  }
 }
 
 export default NavbarComponent
